04183d79f3c1f0fa7e2140fbe6cbf44256af92ab
Arraying <paul.huebner@googlemail.com>
2023-04-26 11:48:39 +0200

    Implement some type checking!

--- lang-import/lib/TypeCheck.hs
+++ lang-import/lib/TypeCheck.hs
@@ -17,17 +17,17 @@ data Label
   deriving (Show, Eq)
 
 data Decl
-  = Decl String Type -- Variable declaration.
+  = Var String Type -- Variable declaration.
   | Modl String Sc -- Module declaration.
   deriving (Eq)
 
 
 instance Show Decl where
-  show (Decl x t) = x ++ " : " ++ show t
+  show (Var x t) = x ++ " : " ++ show t
   show (Modl x s) = "module " ++ x ++ " @ " ++ show s
 
 projTy :: Decl -> Type
-projTy (Decl _ t) = t
+projTy (Var _ t) = t
 projTy (Modl _ _) = ModuleT
 
 -- Scope Graph Library Convenience
@@ -40,9 +40,13 @@ new = S.new @_ @Label @Decl
 sink :: Scope Sc Label Decl < f => Sc -> Label -> Decl -> Free f ()
 sink = S.sink @_ @Label @Decl
 
--- Regular expression P*D
+-- Regular expression (P|I)*D
 re :: RE Label
-re = Dot (Star $ Atom P) $ Atom V
+re = Dot (Star $ Pipe (Atom P) (Atom I)) $ Atom V
+
+-- Regular expression P*Mod
+re' :: RE Label
+re' = Dot (Star $ Atom P) $ Atom M
 
 -- Path order based on length
 pShortest :: PathOrder Label Decl
@@ -50,16 +54,56 @@ pShortest p1 p2 = lenRPath p1 < lenRPath p2
 
 -- Match declaration with particular name
 matchDecl :: String -> Decl -> Bool
-matchDecl x (Decl x' _) = x == x'
+matchDecl x (Var x' _) = x == x'
 matchDecl x (Modl x' _) = x == x'
 
 -- Phase 1: create graph of modules and imports.
 phase1 :: (Functor f, Error String < f, Scope Sc Label Decl < f) => Prog -> Sc -> Free f ()
-phase1 _ g = undefined
+phase1 mods g = mapM_ modl mods
+  where
+    -- This will return the scope of every module, not sure if this is needed for type checking or not. Probably not.
+    modl :: (Functor f, Error String < f, Scope Sc Label Decl < f) => Module -> Free f Sc
+    modl (Module x is _) = do
+      -- Create the new scope for this particular module, link it to the parent.
+      g' <- new
+      edge g' P g
+      -- We want to first handle imports, otherwise we can get into nasty cycles.
+      mapM_ (`impt` g') is
+      -- Now "register" the module with the parent.
+      sink g M $ Modl x g'
+      return g'
+    -- This will attempt to create a singular import edge given a specific "current" scope.
+    impt :: (Functor f, Error String < f, Scope Sc Label Decl < f) => String -> Sc -> Free f ()
+    impt name g' = do
+      -- Let's get all the search results.
+      ds <- query g' re' pShortest (matchDecl name)
+      -- We need to match to determine ambiguity.
+      case ds of
+        [] -> err $ "A module by the name of '" ++ name ++ "' does not exist"
+        [Modl _ g''] -> do
+          -- Draw an edge from our current module g' to the imported g''.
+          edge g' I g''
+        _ -> err $ "Multiple modules by the name of '" ++ name ++ "' exist"
+      
 
 -- Phase 2: perform type checking of all expressions.
 phase2 :: (Functor f, Error String < f, Scope Sc Label Decl < f) => Expr -> Sc -> Free f Type
-phase2 _ g = undefined
+phase2 (Num _) _ = return NumT
+phase2 (Ident x) g = do
+  ds <- query g re pShortest (matchDecl x) <&> map projTy
+  case ds of
+    []  -> err $ "No matching declarations found for '" ++ x ++ "'"
+    [t] -> return t
+    _   -> err "BUG: Multiple declarations found" -- cannot happen for STLC
+phase2 (VarDecl k v) g = do
+  -- First resolve the type (spoiler alert, number).
+  t <- phase2 v g
+  -- Add the declaration to the current scope.
+  g' <- new
+  edge g' P g
+  sink g' V $ Var k t
+  -- Type!
+  return t
 
 -- Perform the actual type checker.
 tc :: (Functor f, Error String < f, Scope Sc Label Decl < f) => Prog -> Sc -> Free f [[Type]]
--- lang-import/tests/Main.hs
+++ lang-import/tests/Main.hs
@@ -10,15 +10,39 @@ import Free.Scope (Graph)
 runTCTest :: Prog -> IO ([[Type]], Graph Label Decl) 
 runTCTest = either assertFailure return . runTC
 
--- Define your test cases like the following
-testApplicationPlus :: IO ()
-testApplicationPlus = do
-  assertEqual "Test" 1 1
+testEmpty :: IO ()
+testEmpty = do
+  t <- runTCTest []
+  assertEqual "Not empty" ([] :: [[Type]]) $ fst t
+
+testBasicSingle :: IO ()
+testBasicSingle = do
+  t <- runTCTest [Module "foo" [] [Num 1, Num 2, Num 3]]
+  assertEqual "Incorrect types" [[NumT, NumT, NumT]] $ fst t 
+
+testBasicMultiple :: IO ()
+testBasicMultiple = do
+  t <- runTCTest [Module "foo" [] [Num 1, Num 2, Num 3], Module "bar" [] [Num 4]]
+  assertEqual "Incorrect types" [[NumT, NumT, NumT], [NumT]] $ fst t 
+
+testDeclSimple :: IO ()
+testDeclSimple = do
+  t <- runTCTest [Module "foo" [] [Num 1, VarDecl "x" $ Num 2]]
+  assertEqual "Incorrect types" [[NumT, NumT]] $ fst t 
+
+testModules :: IO ()
+testModules = do
+  t <- runTCTest example
+  assertEqual "Incorrect types" [[NumT], [NumT]] $ fst t
+
 
 tests :: Test
 tests = TestList
-    -- Add your test cases to this list
-    [ "testApplicationPlus" ~: testApplicationPlus ]
+    [ "testEmpty" ~: testEmpty
+    , "testBasicSingle" ~: testBasicSingle
+    , "testBasicMultiple" ~: testBasicSingle
+    , "testDeclSimple" ~: testDeclSimple
+    , "testModules" ~: testModules ]
 
 main :: IO ()
 main = do
